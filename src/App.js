8d9b3eaaaf4ad995f888dcf7def21a9fe53e98aa
navi107 <naveensing575@gmail.com>
2021-02-17 14:10:41 +0530

    Second comitt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Board from '/src/Board';
 
-export default () => (
-    <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-    <p>Hello my name is Naveen Singh</p>
-    </>
-);
+const App = () =>{
+    return(
+    <div>
+    <h1>TIC TAC TOE</h1>
+    <Board />
+    </div>
+    );
+};
+
+export default App;
---
+++ src/Board.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Square from '/src/Square';
+const board = () => 
+{
+  return (
+    <div>
+      <div>
+        <Square value={0}/>
+        <Square value={1}/>
+        <Square value={2}/>
+      </div>
+      <div>
+        <Square value={3}/>
+        <Square value={4}/>
+        <Square value={5}/>
+      </div>
+      <div>
+        <Square value={6}/>
+        <Square value={7}/>
+        <Square value={8}/>
+      </div>
+    </div>
+  );
+};
+export default board;
