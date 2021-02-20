2d9310fb8b27045555d86e8c76bbea867224f548
navi107 <naveensing575@gmail.com>
2021-02-20 08:54:02 +0530

    styles included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Board from './components/Board';
-import "./Style/root.css";
+import "./Style/root.scss";
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1>TIC TAC TOE</h1>
       <Board />
     </div>
